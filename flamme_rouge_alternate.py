119cc629a2c64204896738eb765a57a4825c5550
Leon Osborne <leonosborne20@gmail.com>
2020-03-24 18:17:15 +0000

    updated select doesnt discard all cards

--- flamme_rouge_alternate.py
+++ flamme_rouge_alternate.py
@@ -20,42 +20,87 @@ class Player():
         
         self.draw_count = 4
         
-        self.roller_draw = Deck({3:3, 4:3, 5:3, 6:3, 7:3})
-        self.roller_draw.shuffle()
-        self.roller_removed = []
-        self.roller_hand = []
-        self.roller_discard = []
+        # Make the roller and sprinter cards
+        self.roller_cards = self.build({3:3, 4:3, 5:3, 6:3, 7:3})
+        self.sprinter_cards = self.build({2:3, 3:3, 4:3, 5:3, 9:3})
         
-        self.sprinter_draw = Deck({2:3, 3:3, 4:3, 5:3, 9:3})
-        self.sprinter_draw.shuffle()
-        self.sprinter_removed = []
-        self.sprinter_hand = []
-        self.sprinter_discard = []
+        # Create roller card areas
+        self.roller_draw = Deck()
+        self.roller_draw.add_to_deck(self.roller_cards)
+        self.roller_draw.shuffle()        
+        self.roller_removed = Deck()
+        self.roller_hand = Deck()
+        self.roller_select = Deck()
+        self.roller_discard = Deck()
         
-        self.selected_cards = {'roller': [], 'srinter': []}
+        #Create sprinter card areas
+        self.sprinter_draw = Deck()
+        self.sprinter_draw.add_to_deck(self.sprinter_cards)
+        self.sprinter_draw.shuffle()        
+        self.sprinter_removed = Deck()
+        self.sprinter_hand = Deck()
+        self.sprinter_select = Deck()
+        self.sprinter_discard = Deck()
+        
+        
 
-    def get_deck_info(self):
-        return self.deck_setups[self.p_type]
+    def build(self, deck_dict):
+        card_list = []
+        for k, v in deck_dict.items():
+            for x in range(1, v + 1):
+                card_list.append(Card(k))
+        return card_list
 
-    def setup_shuffle(self):
-        for d in self.draw_decks:
-            self.draw_decks[d].shuffle()
-        
-    def get_draw_amount(self):
-        return self.draw_count[self.p_type]
 
     def check_remain(self, deck):
-        return len(deck)
+        return len(deck.cards)
 
-    def draw_hand(self, hand, deck, draw_amount):
-        for d in range(1, draw_amount):
-            hand.append(deck.draw_card())
-        print(hand)
+    def draw_hand(self, hand, oppo_hand, deck, discard, draw_amount):
+        if self.check_remain(oppo_hand) > 0:
+            print("Must select a card from your drawn hand before drawing\
+                  from this deck.")
+        else:
+            for d in range(1, draw_amount + 1):
+                if self.check_remain(deck) <= 0:
+                    if self.check_remain(discard) <= 0:
+                        break
+                    else:
+                        self.reshuffle_discard(discard, deck)
+                hand.cards.append(deck.draw_card())
+            
+    def reshuffle_discard(self, discard, deck):
+        for c in discard.cards:
+            deck.cards.append(discard.draw_card())
+        deck.shuffle()
+    
+    def select_card_to_play(self, hand, select, discard):
+        selecting = True
+        while selecting:
+            print('Type your chosen value')
+            chosen_value = input()
+            if chosen_value == 'q':
+                break
+            else:
+                chosen_value = int(chosen_value)
+            for c in hand.cards:
+                if chosen_value == c.value:
+                    select.cards.append(hand.draw_card(hand.cards.index(c)))
+                    selecting = False
+                    break
+            if selecting == False:
+                # Dump remaing hand to discard
+                for c in hand.cards:
+                    discard.cards.append(hand.draw_card(0))
+            else:
+                print("I couldn't find that card. Please try and enter the \
+                      value again. Or enter q to exit.")
+                
+        
 
-    def select_card(self, hand, remove, discard):
-        print('Pick a value to play')
-        select = input()
-        self.choose_card()
+#    def select_card(self, hand, remove, discard):
+#        print('Pick a value to play')
+#        select = input()
+#        self.choose_card()
 
 class Muscle_Team:
     def __init__(self): 
@@ -82,20 +127,16 @@ class Peloton_Team:
         
         self.roller_draw = Deck({3:3, 4:3, 5:3, 6:3, 7:3, 'attack!':2})
         self.roller_draw.shuffle()
+        
+        self.roller_draw = []
         self.roller_removed = []
         self.roller_hand = []
         self.roller_discard = []
 
 
 class Deck:
-    def __init__(self, deck_dict = {}):
+    def __init__(self):
         self.cards = []
-        self.build(deck_dict)
-
-    def build(self, deck_dict):
-        for k, v in deck_dict.items():
-            for x in range(1, v + 1):
-                self.cards.append(Card(k))
     
     def show(self):
         for c in self.cards:
@@ -106,9 +147,12 @@ class Deck:
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
     
-    def draw_card(self):
-        return self.cards.pop()
+    def draw_card(self, pos = 0):
+        return self.cards.pop(pos)
 
+    def add_to_deck(self, list_of_cards):
+        for c in list_of_cards:
+            self.cards.append(c)
 
 
 class Card:
